f169a147ecc1f7567ef7382554035d02fe5aea09
CrossChEp <61206698+CrossChEp@users.noreply.github.com>
2021-02-16 16:21:41 +0300

    Update Source2.cpp

--- Source2.cpp
+++ Source2.cpp
@@ -14,7 +14,8 @@ enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
 eDirection dir;
 
 
-void Setup() {
+void Setup() 
+{
 
 	game_over = false;
 	dir = STOP;
@@ -25,55 +26,69 @@ void Setup() {
 	score = 0;
 }
 
-void Draw() {
+void Draw() 
+{
 	system("cls");
-	for (int i = 0; i < Width+1; i++) {
-		
-			cout << "#";
-		}
-	cout << endl;
-
-	for (int i = 0; i < Height; i++) {
-		for (int j = 0; j < Width; j++) {
-			if (j == 0 || j == Width - 1) {
-				cout << "#";
-			}
-			if (i == y && j == x) {
-				
-				cout << "0";
-			}
-			else if (i == fruity && j == fruitx) {
-				cout << "F";
-			}
-			else {
-				bool PrintTail = false;
-				for (int k = 0; k < nTail; k++) {
-					if (tailx[k] == j && taily[k] == i) {
-						PrintTail = true;
-						cout << "o";
-					}
-				}
-				if (!PrintTail) {
-					cout << " ";
-				}
-			}
-			
-		}
-		cout << endl;
-	}
-
-	for (int i = 0; i < Width+1; i++) {
+	for (int i = 0; i < Width + 1; i++) 
+	{
 
 		cout << "#";
 	}
 	cout << endl;
-	cout << "Score: "<<score<<endl;
+
+	for (int i = 0; i < Height; i++) 
+	{
+		for (int j = 0; j < Width; j++) 
+		{
+			if (j == 0 || j == Width - 1) 
+			{
+				cout << "#";
+			}
+			if (i == y && j == x) 
+			{
+
+				cout << "0";
+			}
+			else if (i == fruity && j == fruitx) 
+			{
+				cout << "F";
+			}
+			else {
+				bool PrintTail = false;
+				for (int k = 0; k < nTail; k++) 
+				{
+					if (tailx[k] == j && taily[k] == i) 
+					{
+						PrintTail = true;
+						cout << "o";
+					}
+				}
+				if (!PrintTail) 
+				{
+					cout << " ";
+				}
+			}
+
+		}
+		cout << endl;
+	}
+
+	for (int i = 0; i < Width + 1; i++) 
+	{
+
+		cout << "#";
+	}
+	cout << endl;
+	cout << "Score: " << score << endl;
 
 }
 
-void Keyboard() {
-	if (_kbhit()) {
-		switch (_getch()) {
+void Keyboard()
+{
+	if (_kbhit()) 
+	{
+		switch (_getch()) 
+		{
 		case 'a':
 			dir = LEFT;
 			break;
@@ -92,16 +107,18 @@ void Keyboard() {
 		}
 	}
 
-	
+
 }
 
-void Logic() {
+void Logic() 
+{
 	int prevx = tailx[0];
 	int prevy = taily[0];
 	int prev2x, prev2y;
 	tailx[0] = x;
 	taily[0] = y;
-	for (int i = 1; i < nTail; i++) {
+	for (int i = 1; i < nTail; i++) 
+	{
 		prev2x = tailx[i];
 		prev2y = taily[i];
 		tailx[i] = prevx;
@@ -110,7 +127,8 @@ void Logic() {
 		prevy = prev2y;
 
 	}
-	switch (dir) {
+	switch (dir) 
+	{
 	case LEFT:
 		x--;
 		break;
@@ -124,52 +142,60 @@ void Logic() {
 		y++;
 		break;
 	}
-	if (x >= Width || x<=0 || y>=Height || y <= 0) {
+	if (x >= Width || x <= 0 || y > Height || y < 0) 
+	{
 		game_over == true;
-		cout << "Âû ïðîãðàëè :(\n";
+		cout << "Ð’Ñ‹ Ð¿Ñ€Ð¾Ð¸Ð³Ñ€Ð°Ð»Ð¸ :(\n";
 		system("pause");
 	}
-	if (x >= Width) {
+	if (x >= Width) 
+	{
 		x = 0;
 	}
-	else if (x < 0) {
+	else if (x < 0) 
+	{
 		x = Width - 1;
 	}
-	if (y >= Height) {
+	if (y >= Height) 
+	{
 		y = 0;
 	}
-	else if (y < 0) {
+	else if (y < 0) 
+	{
 		Height - 1;
 	}
-	
-	if (x == fruitx && y == fruity) {
-		
+
+	if (x == fruitx && y == fruity) 
+	{
+
 		score += 10;
-		fruitx = rand()  % Width;
+		fruitx = rand() % Width;
 		fruity = rand() % Height;
 		nTail++;
 
 
 	}
-	
-	
+
+
 }
 
 
 
-int main() {
-	
-		setlocale(LC_ALL, "rus");
+int main() 
+{
 
-		Setup();
+	setlocale(LC_ALL, "rus");
 
-		while (!game_over) {
-			Draw();
-			Keyboard();
-			Logic();
+	Setup();
 
-		}
+	while (!game_over) 
+	{
+		Draw();
+		Keyboard();
+		Logic();
 
-		system("pause");
-	
+	}
+
+	system("pause");
+
 }
